13615f52e565e33414a2f2ef0e75ba69cf8a0318
phpwdk <9653992@qq.com>
2021-07-27 13:23:28 +0800

    修改测试方式

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def calculated_weight(weight):
 def main():
     com = COMMON()
     # 初始化秤
+    GPIO.cleanup()
     hx = HX711(dout, pd_sck)
     hx.set_reading_format("MSB", "MSB")
     hx.set_reference_unit(referenceUnit)
@@ -29,17 +30,20 @@ def main():
     response = com.get(url)
     if not response:
         print("访问网络失败")
-    print(response)
+    # print(response)
     is_switch = response['data']['switch']
     peeled = response['data']['peeled']
+    old_weight = 0
     # 循环读取数值
     while 1:
         try:
             weight = 0
+            p = 0
             # 去皮
             if peeled == 1:
                 hx.reset()
                 hx.tare()
+                p = 1
             else:
                 # 断电休眠
                 if is_switch == 1:
@@ -54,12 +58,17 @@ def main():
                     print("weight: %d" % weight)
             # 上报设备数据
             api_url_path = "https://disc.wkh01.top/device/weigh/v1"
-            url = "%s?serial=%s&weight=%d" % (api_url_path, com.serial(), weight)
+            url = "%s?serial=%s&weight=%d&peeled=%d" % (api_url_path, com.serial(), weight, p)
             response = com.get(url)
             if response:
                 is_switch = response['data']['switch']
                 peeled = response['data']['peeled']
-            time.sleep(3)
+            if old_weight != weight:
+                stop_time = 0.1
+                old_weight = weight
+            else:
+                stop_time = 3
+            time.sleep(stop_time)
 
         except (KeyboardInterrupt, SystemExit):
             cleanAndExit()
